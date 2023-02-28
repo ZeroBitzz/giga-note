5f2fb9c0215e239054e41bfc7c63aa47fff9706f
Broderick <zerothesu@gmail.com>
2023-02-28 14:10:35 -0700

    fixed mime checking error

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const ejs = require('ejs')
 const app = express()
 const port = 3000
 
+app.use(express.static(__dirname + '/public')); // load static files from public directory
+
 // set view engine
 app.set('view engine', 'ejs')
+
 
 // home route
 app.get('/', function(req, res){
